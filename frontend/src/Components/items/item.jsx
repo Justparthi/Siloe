e7c768247bdcbbe809b3e62627f9e7e37b44834d
Parthiban <155973403+Justparthi@users.noreply.github.com>
2025-01-25 23:38:36 +0530

    Update item.jsx

--- frontend/src/Components/items/item.jsx
+++ frontend/src/Components/items/item.jsx
@@ -5,20 +5,28 @@ import { Link } from 'react-router-dom';
 const Item = (props) => {
     return (
         <div className='item'>
-            <div class="card">
-                <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,10)} src={props.image} class="card-img-top" alt="..." /></Link>
-                <div class="card-body">
-                <p class="card-text">{props.name}</p>
-                <div className='item-prices'>
-                <div className='item-price-new'>
-                    $ {props.new_price}
-                </div>
-                <div className='item-price-old' style={{textDecoration : 'line-through'}}>
-                    {props.old_price}
+            <div className="card">
+                <Link to={`/product/${props.id}`}>
+                    <img 
+                        onClick={() => window.scrollTo(0,10)} 
+                        src={props.image} 
+                        className="card-img-top" 
+                        alt={props.name} 
+                    />
+                </Link>
+                <div className="card-body">
+                    <p className="card-text">{props.name}</p>
+                    <div className='item-prices'>
+                        <div className='item-price-new'>
+                            $ {props.new_price}
+                        </div>
+                        <div className='item-price-old'>
+                            $ {props.old_price}
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
-  </div>
-</div>
         </div>
     );
 }
