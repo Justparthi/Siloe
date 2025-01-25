bb627333d7100932f2d698c8a31d3a1354e696f1
Parthiban <155973403+Justparthi@users.noreply.github.com>
2025-01-25 23:37:48 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -4,32 +4,40 @@ import './Popular.css'
 import Item from '../items/item.jsx';
 
 const Popular = () => {
-
     const [popular_product, setPopular_Product] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://postecom-backend.onrender.com/popularwomen')
         .then((response) => response.json())
-        .then((data) => setPopular_Product(data));
+        .then((data) => {
+            setPopular_Product(data);
+            setIsLoading(false);
+        })
+        .catch((error) => {
+            console.error('Error fetching products:', error);
+            setIsLoading(false);
+        });
     },[])
-
-
 
     return (
         <div className='popular'>
             <h1>Popular in Women</h1>
             <hr />
             <div className='popular-item'>
-                {popular_product.map((item, i) =>{
-                    return <Item 
-                    key={i}
-                    id={item.id}
-                    name={item.name}
-                    image={item.image}
-                    new_price={item.new_price}
-                    old_price={item.old_price}/>
-                })}
-
+                {isLoading ? (
+                    <div className='loading'>Loading...</div>
+                ) : (
+                    popular_product.map((item, i) =>(
+                        <Item 
+                        key={i}
+                        id={item.id}
+                        name={item.name}
+                        image={item.image}
+                        new_price={item.new_price}
+                        old_price={item.old_price}/>
+                    ))
+                )}
             </div>
         </div>
     );
