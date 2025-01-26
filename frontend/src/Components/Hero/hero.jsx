2f635211fe9447979a67d1fdc9025aabdbf96f05
Parthiban <155973403+Justparthi@users.noreply.github.com>
2025-01-26 21:43:56 +0530

    Update hero.jsx

--- frontend/src/Components/Hero/hero.jsx
+++ frontend/src/Components/Hero/hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                             <p>Redefining style through conscious design and innovative materials.</p>
                             
                             <div className="hero-action-group">
-                                <Link to="/collections" className="hero-cta">
+                                <Link to="/mens" className="hero-cta">
                                     Explore Collections
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
