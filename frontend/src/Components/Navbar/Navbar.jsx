2c379ac458ac15180179d6a012aada85481670da
Parthiban <155973403+Justparthi@users.noreply.github.com>
2025-01-28 07:37:09 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 import cartIcon from '../../assets/cart_icon.png' 
